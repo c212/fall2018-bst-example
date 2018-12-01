426c8a5526abca4b2318cab1513ed64c48149138
c212 <40395648+c212@users.noreply.github.com>
2018-12-01 10:24:44 -0500

    Additional testing helps us find errors.
    Small error in find is exposed (and corrected) at this early stage.

--- BST.java
+++ BST.java
@@ -10,11 +10,14 @@ public class BST {
     BST a = new BST(8, null, null);  
     System.out.println( a ); // expect 8
     System.out.println( a.find(8) ); // true
-    System.out.println( a.find(12) ); // false
+    System.out.println( a.find(7) ); // false
+    a.left = new BST(7, null, null); 
+    System.out.println( a ); // 7  8
+    System.out.println( a.find(7) ); // true    
   }
   public boolean find(int value) {
     if (this.value == value) return true;
-    else if (this.value < value) 
+    else if (this.value > value) // this was wrong, fixed now
       return (this.left == null) ? false : this.left.find(value); 
     else // this.value > value 
       return (this.right == null) ? false : this.right.find(value); 
