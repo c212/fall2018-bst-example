5247a5227e2747a2907a7a1e38cd0f7d04d87173
c212 <40395648+c212@users.noreply.github.com>
2018-12-01 09:48:06 -0500

    Start describing our data representation
    This is a simple recursive data structure.

--- BST.java
+++ BST.java
@@ -1,3 +1,12 @@
 public class BST {
-  
+  int value; 
+  BST left, right;
+  public BST(int value, BST left, BST right) {
+    this.value = value; 
+    this.left = left; 
+    this.right = right; 
+  }
+  public static void main(String[] args) {
+    BST a = new BST(8, null, null);  
+  }
 }
