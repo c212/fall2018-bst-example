9e0803bee9fc5faa2ae31b7eb755627d9d78687a
c212 <40395648+c212@users.noreply.github.com>
2018-12-01 09:58:48 -0500

    Starting unit testing.
    Adding a toString() method to start viewing these structures.

--- BST.java
+++ BST.java
@@ -8,5 +8,11 @@ public class BST {
   }
   public static void main(String[] args) {
     BST a = new BST(8, null, null);  
+    System.out.println( a ); // expect 8
+  }
+  public String toString() {
+    String left = (this.left  == null) ? "" : this.left.toString(), 
+          right = (this.right == null) ? "" : this.right.toString();
+    return left + " " + this.value + " " + right;     
   }
 }
